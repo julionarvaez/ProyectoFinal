c882639e26e4e3c8b2742b39c11d876bd781950f
Julio Narvaez <juliocesarnarvaez30@gmail.com>
2025-04-28 03:43:24 -0500

    Configura el app-routing.module.ts y Configura el auth.service.ts

--- mi-proyecto/frontend/frontend/src/app/app.routes.ts
+++ mi-proyecto/frontend/frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'perfil', component: PerfilComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
